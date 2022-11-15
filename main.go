42dea9959ae181913e2f9f8c0c6262a1acd69a0a
Malachy <malachyguitar@gmail.com>
2022-11-15 12:02:40 +0000

    Downloaded some files and started work

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+
 	tools "github.com/malachyoconnor/MQTT-GO/tools"
+)
+
+var (
+	exampleHex = "18000000600edcea002206800000000000000000000000000000000100000000000000000000000000000001ec38075b4060bda33524ffff501827f6ab170000100c00044d5154540402003c0000"
 )
 
 func main() {
 	fmt.Println("?")
 	tools.Test()
+	tools.DecodeHex(exampleHex)
 }
--- tools/decode.go
+++ tools/decode.go
@@ -1,9 +1,22 @@
 package tools
 
 import (
+	h "encoding/hex"
 	"fmt"
 )
 
 func Test() {
 	fmt.Println("??")
+}
+
+func DecodeHex(hex string) {
+	result, err := h.DecodeString(hex)
+
+	for x := range result {
+		fmt.Printf("%b\n", x)
+	}
+
+	fmt.Println(result, err)
+
+	fmt.Println("Got here")
 }
