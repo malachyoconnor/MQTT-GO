9e5897e3891c28c9f5ef8f344cfa441fe72df2f9
malachyoconnor <malachyguitar@gmail.com>
2023-01-03 15:12:10 +0000

    feat(gobro): Added tickets and waiting on tickets to the messageHandler. Also changed the messageHandler so that messages are all sent to the MessageSender in one place - so that we can pass the tickets there too.

--- gobro/messageHandler.go
+++ gobro/messageHandler.go
@@ -26,9 +26,9 @@ func (msgH *MessageHandler) Listen(server *Server) {
 	for {
 		clientMessage := <-(*msgH.AttachedInputChan)
 		clientID := *clientMessage.ClientID
-		// Could be nil if the client doesn't exist yet
-		client := (*clientTable)[clientID]
+		client := clientTable.Get(clientID)
 
+		ticket := client.Tickets.GetTicket()
 		packetArray := clientMessage.Packet
 		packet, packetType, err := packets.DecodePacket(*packetArray)
 
@@ -36,10 +36,10 @@ func (msgH *MessageHandler) Listen(server *Server) {
 			fmt.Println(err)
 			continue
 		}
-		// General case for if the client doesn't exist IF NOT A CONNECT packet
+		// General case for if the client doesn't exist if NOT a connect packet
 		if packetType != packets.CONNECT {
 
-			if _, found := (*clientTable)[clientID]; !found {
+			if !clientTable.Exists(clientID) {
 				fmt.Println("Client not in the client table sent", packets.PacketTypeName(packetType), "message, disconnecting.")
 				// If the client hasn't already been disconnected by the client handler
 				if client != nil {
@@ -50,32 +50,28 @@ func (msgH *MessageHandler) Listen(server *Server) {
 
 		}
 
-		go HandleMessage(packetType, packet, client, server, clientMessage)
+		go HandleMessage(packetType, packet, client, server, clientMessage, ticket)
 
 	}
 
 }
 
-func HandleMessage(packetType byte, packet *packets.Packet, client *clients.Client, server *Server, clientMessage clients.ClientMessage) {
+func HandleMessage(packetType byte, packet *packets.Packet, client *clients.Client, server *Server, clientMessage clients.ClientMessage, ticket structures.Ticket) {
 	clientTable := server.clientTable
 	clientID := *clientMessage.ClientID
 	clientConnection := *clientMessage.ClientConnection
-	if client != nil {
-		client.Queue.DoingWork()
-	}
+	packetsToSend := make([]*clients.ClientMessage, 0, 10)
 
 	switch packetType {
 	case packets.CONNECT:
 		// Query the client table to check if the client exists
 		// if not slap it in there - then send connack
 
-		// This should disconnect them if they're already connected !!
-		createClient(clientTable, &clientMessage)
 		// Check if the reserved flag is zero, if not disconnect them
 		// Finally send out a CONACK [X]
-		connackArray := packets.CreateConnACK(false, 0)
-		clientMsg := clients.CreateClientMessage(clientID, &clientConnection, &connackArray)
-		(*server.outputChan) <- clientMsg
+		connack := packets.CreateConnACK(false, 0)
+		clientMsg := clients.CreateClientMessage(clientID, &clientConnection, &connack)
+		packetsToSend = append(packetsToSend, &clientMsg)
 
 	case packets.PUBLISH:
 		fmt.Println("Received request to publish:", string(packet.Payload.ApplicationMessage[:]))
@@ -85,11 +81,8 @@ func HandleMessage(packetType byte, packet *packets.Packet, client *clients.Clie
 			TopicFilter: varHeader.TopicFilter,
 			Qos:         packet.ControlHeader.Flags & 6,
 		}
-		handlePublish(server.topicClientMap, topic, clientMessage, server.outputChan, server.clientTable)
-
-		// err := handlePublish(server.topicClientMap,)
-		// Get the clients connected to that topic and send them
-		// all a lovely packet
+		// Adds to the packets to send
+		handlePublish(server.topicClientMap, topic, clientMessage, server.outputChan, server.clientTable, &packetsToSend)
 
 	case packets.SUBSCRIBE:
 		fmt.Println("Handling subscribe")
@@ -109,22 +102,29 @@ func HandleMessage(packetType byte, packet *packets.Packet, client *clients.Clie
 		packetID := packet.VariableLengthHeader.(packets.SubscribeVariableHeader).PacketIdentifier
 		subackPacket := packets.CreateSubACK(packetID, returnCodes)
 		clientMsg := clients.CreateClientMessage(clientID, &clientConnection, &subackPacket)
-		(*server.outputChan) <- clientMsg
+		packetsToSend = append(packetsToSend, &clientMsg)
 
 	case packets.DISCONNECT:
 		// Close the client TCP connection.
 		// Remove the packet from the client list
+		ticket.WaitOnTicket()
 		fmt.Println("Disconnecting", clientID)
-		clientConnection.Close()
-
-		// TODO - do this centrally.
-
-		delete(*clientTable, clientID)
+		client.Disconnect(server.topicClientMap, clientTable)
+		ticket.TicketCompleted()
+		return
 	}
 
-	if (packetType != packets.PUBLISH) && (client != nil) {
-		client.Queue.FinishedWork()
+	fmt.Println("Waiting for ticket to complete for", clientID)
+	ticket.WaitOnTicket()
+	fmt.Println("ticket complete for", clientID)
+	for _, packet := range packetsToSend {
+
+		(*server.outputChan) <- *packet
+
 	}
+	ticket.TicketCompleted()
+
+	fmt.Println()
 }
 
 // Decode topics and store them in subscription table
@@ -154,7 +154,8 @@ func handleSubscribe(topicClientMap *clients.TopicToClient, client *clients.Clie
 		offset += utfStringLen + 1
 
 		if _, found := (*topicClientMap)[topic]; !found {
-			(*topicClientMap)[topic] = structures.CreateLinkedList[clients.ClientID]()
+			newLL := structures.CreateLinkedList[clients.ClientID]()
+			(*topicClientMap)[topic] = &newLL
 		}
 	}
 
@@ -171,36 +172,24 @@ func handleSubscribe(topicClientMap *clients.TopicToClient, client *clients.Clie
 		topicClientMap.AddTopicClientPair(newTopic, client.ClientIdentifier)
 	}
 
-	fmt.Println(client.Topics, *topicClientMap)
-
 	return newTopics, nil
 
 }
 
-func handlePublish(TCMap *clients.TopicToClient, topic clients.Topic, msgToForward clients.ClientMessage, outputChannel *chan clients.ClientMessage, clientTable *clients.ClientTable) {
-	return
-	// clientList := (*TCMap)[topic]
+func handlePublish(TCMap *clients.TopicToClient, topic clients.Topic, msgToForward clients.ClientMessage, outputChannel *chan clients.ClientMessage, clientTable *clients.ClientTable, toSend *[]*clients.ClientMessage) {
+	clientList := (*TCMap)[topic]
+	if clientList == nil {
+		return
+	}
+	node := clientList.Head()
 
-	// for i := range clientList {
-
-	// 	clientID := clientList[i]
-	// 	alteredMsg := msgToForward
-	// 	alteredMsg.ClientID = &clientID
-	// 	alteredMsg.ClientConnection = &(*clientTable)[clientID].TCPConnection
-
-	// 	(*outputChannel) <- alteredMsg
-	// }
-
-}
-
-func createClient(clientTable *clients.ClientTable, clientMsg *clients.ClientMessage) *clients.Client {
-
-	if client, found := (*clientTable)[*clientMsg.ClientID]; found {
-		return client
+	for node != nil {
+		clientID := node.Value()
+		alteredMsg := msgToForward
+		alteredMsg.ClientID = &clientID
+		alteredMsg.ClientConnection = &(clientTable.Get(clientID).TCPConnection)
+		(*toSend) = append(*toSend, &alteredMsg)
+		node = node.Next()
 	}
 
-	client := clients.CreateClient(*clientMsg.ClientID, clientMsg.ClientConnection)
-	(*clientTable)[*clientMsg.ClientID] = &client
-
-	return &client
 }
