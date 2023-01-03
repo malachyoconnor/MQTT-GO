2253779515bc3d63042131c8f8f8af0579e015a8
malachyoconnor <malachyguitar@gmail.com>
2023-01-03 15:15:21 +0000

    feat(MQTT-GO): Added pprof CPU profiling.

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
-import "MQTT-GO/gobro"
+import (
+	"MQTT-GO/gobro"
+	"flag"
+	"fmt"
+	"os"
+	"runtime/pprof"
+	"time"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
+
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			fmt.Println(err)
+		}
+		pprof.StartCPUProfile(f)
+
+		go func() {
+			fmt.Println("STARTING PROFILING")
+			time.Sleep(60 * time.Second)
+			pprof.StopCPUProfile()
+			fmt.Println("FINISHED PROFILING")
+		}()
+	}
 
 	server := gobro.CreateServer()
 	server.StartServer()
