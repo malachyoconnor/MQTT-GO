a0d9f7b9349a953fc934d00b2935f29df6b273c3
malachyoconnor <malachyguitar@gmail.com>
2023-01-07 16:32:32 +0000

    feat(client): Getting the client to connect. Started work on client actions, the client now connects and sends gibberish.

--- client/clientActions.go
+++ client/clientActions.go
@@ -1,3 +1,21 @@
 package client
 
+import (
+	"fmt"
+	"net"
+)
+
 // Here we'll have the functions that make the client perform it's actions.
+
+func ConnectToServer(ip string, port int) {
+	address := net.JoinHostPort(ip, fmt.Sprint(port))
+
+	connection, err := net.Dial("tcp", address)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	connection.Write([]byte{123})
+}
--- client/clientRun.go
+++ client/clientRun.go
@@ -1,21 +1,35 @@
 package client
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
-var port = flag.Int("port", 8000, "Get the port the server is being run on")
+var (
+	port = flag.Int("port", 8000, "Get the port the server is being run on")
+	ip   = flag.String("ip", "localhost", "Get the ip the server is being run on")
+)
 
 // We want to listen on the command line for inputs
 // E.g. Subscribe, publish, disconnect etc.
 // Then send values to the server
 func StartClient() {
 	flag.Parse()
+	fmt.Println("Connecting to server on port", *port)
 
-	args := os.Args[1:]
-	fmt.Println(args)
-	fmt.Println(*port)
+	ConnectToServer(*ip, *port)
 
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		fmt.Println(text)
+
+	}
 }
