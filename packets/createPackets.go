7607589fde4c78f41cf49d59534f2d0d40d17540
malachyoconnor <malachyguitar@gmail.com>
2023-01-08 14:05:26 +0000

    feat(packets): An now to encode CONNECT packets. Also added a CombinePacketSections function to reduce writing.

--- packets/createPackets.go
+++ packets/createPackets.go
@@ -1,5 +1,15 @@
 package packets
 
+import "errors"
+
+func CombinePacketSections(controlHeader ControlHeader, varLengthHeader VariableLengthHeader, payload PacketPayload) *Packet {
+	resultPacket := Packet{}
+	resultPacket.ControlHeader = controlHeader
+	resultPacket.VariableLengthHeader = varLengthHeader
+	resultPacket.Payload = payload
+	return &resultPacket
+}
+
 func EncodeFixedHeader(fixedHeader ControlHeader) []byte {
 
 	result := make([]byte, 1)
@@ -54,12 +64,74 @@ func CreateSubACK(packetIdentifier int, returnCodes []byte) []byte {
 	}
 
 	return result
-
 }
 
-func CreateConnect(packet Packet) *[]byte {
+// TODO: We discard errors in this function...
+func CreateConnect(packet *Packet) (*[]byte, error) {
 	if packet.ControlHeader.Type != CONNECT {
 		panic("Tried to create a connect packet from a non-connect packet")
 	}
-	return nil
+	// Need to do control header last because only at the end can we know the value of the remaining length field
+
+	varLengthHeader := packet.VariableLengthHeader.(ConnectVariableHeader)
+	// TODO: Random choice of 30 here - could be improved with some looking into, same for the Payload.
+	resultVarHeader := make([]byte, 0, 30)
+	protocolNameArr, _, _ := EncodeUTFString("MQTT")
+
+	// Version 3.1.1 has a protocol version of 4
+	protocol := byte(4)
+	connectFlags := varLengthHeader.ConnectFlags
+	keepAliveMsb, keepAliveLsb := getMSBandLSB(varLengthHeader.KeepAlive)
+
+	resultVarHeader = append(resultVarHeader, protocolNameArr...)
+	resultVarHeader = append(resultVarHeader, protocol, connectFlags, keepAliveMsb, keepAliveLsb)
+
+	payload := packet.Payload
+	resultPayload := make([]byte, 0, 30)
+	// Client Identifier
+	clientIdentifier, _, err := EncodeUTFString(payload.ClientID)
+	if err != nil {
+		return nil, errors.New("error: clientID not provided")
+	}
+
+	resultPayload = append(resultPayload, clientIdentifier...)
+
+	// Will Topic & Will Message (If the will flag is set to 1)
+	if (varLengthHeader.ConnectFlags & 4) > 0 {
+		if payload.WillTopic != "" || payload.WillMessage == nil {
+			return nil, errors.New("error: Will metadata not provided")
+		}
+		willTopic, _, _ := EncodeUTFString(payload.WillTopic)
+		willMessage, _, _ := EncodeUTFString(payload.WillTopic)
+		resultPayload = append(resultPayload, willTopic...)
+		resultPayload = append(resultPayload, willMessage...)
+	}
+	// User Name
+	if (varLengthHeader.ConnectFlags & 128) > 0 {
+		if payload.Username == "" {
+			return nil, errors.New("error: Username metadata not provided")
+		}
+		username, _, _ := EncodeUTFString(payload.Username)
+		resultPayload = append(resultPayload, username...)
+	}
+
+	// Password
+	if (varLengthHeader.ConnectFlags & 64) > 0 {
+		if payload.Password == nil {
+			return nil, errors.New("error: Password metadata not provided")
+		}
+		passwordLenMSB, passwordLenLSB := getMSBandLSB(len(*payload.Password))
+		resultPayload = append(resultPayload, passwordLenMSB, passwordLenLSB)
+		resultPayload = append(resultPayload, *payload.Password...)
+	}
+
+	packet.ControlHeader.RemainingLength = len(resultPayload) + len(resultVarHeader)
+	resultControlHeader := EncodeFixedHeader(packet.ControlHeader)
+
+	resultPacket := make([]byte, 0, len(resultControlHeader)+len(resultVarHeader)+len(resultPayload))
+	resultPacket = append(resultPacket, resultControlHeader...)
+	resultPacket = append(resultPacket, resultVarHeader...)
+	resultPacket = append(resultPacket, resultPayload...)
+
+	return &resultPacket, nil
 }
