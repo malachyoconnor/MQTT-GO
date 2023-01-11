fb303977f9bbe4cb427608b3475427d4d5892d4f
malachyoconnor <malachyguitar@gmail.com>
2023-01-11 15:15:38 +0000

    feat(client):  +packetIdentifier & waitingPackets. PacketIdentifier is simply a thread safe unique integer which can safely be used for packet identifiers. waitingPackets is a method of waiting on a return message from the broker which includes said packet identifier. It uses sync.Cond to wake up all waiting threads who then go and check if the packet they were waiting on is done with.

---
+++ client/packetIdentifier.go
@@ -0,0 +1,95 @@
+package client
+
+import (
+	"MQTT-GO/structures"
+	"fmt"
+	"sync"
+)
+
+type packetIdStore struct {
+	packetIdentifier int
+	packetIdLock     sync.Mutex
+}
+
+var (
+	packetIdentifier = packetIdStore{
+		packetIdentifier: 0,
+		packetIdLock:     sync.Mutex{},
+	}
+)
+
+// func incrementPacketId() {
+// 	packetIdentifier.packetIdLock.Lock()
+// 	defer packetIdentifier.packetIdLock.Unlock()
+// 	packetIdentifier.packetIdentifier++
+// }
+
+// func getPacketId() int {
+// 	packetIdentifier.packetIdLock.Lock()
+// 	defer packetIdentifier.packetIdLock.Unlock()
+// 	return packetIdentifier.packetIdentifier
+// }
+
+func getAndIncrementPacketId() int {
+	packetIdentifier.packetIdLock.Lock()
+	defer packetIdentifier.packetIdLock.Unlock()
+	packetIdentifier.packetIdentifier++
+	return packetIdentifier.packetIdentifier - 1
+}
+
+// TODO:
+// Basically working on receiving ACKS and waiting on the packet identifier
+
+type waitingPackets struct {
+	packetList    *structures.LinkedList[*storedPacket]
+	waitCondition *sync.Cond
+}
+
+type storedPacket struct {
+	packet   []byte
+	packetID int
+}
+
+func CreateWaitingPacketList() *waitingPackets {
+
+	conditionMutex := sync.Mutex{}
+	waitingPacketStruct := waitingPackets{
+		waitCondition: sync.NewCond(&conditionMutex),
+		packetList:    structures.CreateLinkedList[*storedPacket](),
+	}
+
+	return &waitingPacketStruct
+}
+
+func (wp *waitingPackets) AddItem(storedPacket *storedPacket) {
+	wp.waitCondition.L.Lock()
+	wp.packetList.Append(storedPacket)
+	wp.waitCondition.Broadcast()
+	wp.waitCondition.L.Unlock()
+}
+
+func (wp *waitingPackets) getItem(packetIdentifier int) *[]byte {
+	packetFinder := func(s *storedPacket) bool { return s.packetID == packetIdentifier }
+	packetStore := wp.packetList.FilterSingleItem(packetFinder)
+	if packetStore != nil {
+		return &(*packetStore).packet
+	}
+	return nil
+}
+
+func (wp *waitingPackets) GetOrWait(packetIdentifier int) *[]byte {
+
+	wp.waitCondition.L.Lock()
+	for {
+		fmt.Print("AWAKE", packetIdentifier)
+		storedPacket := wp.getItem(packetIdentifier)
+		if storedPacket == nil {
+			fmt.Println()
+			wp.waitCondition.Wait()
+		} else {
+			fmt.Println("Got the item")
+			wp.waitCondition.L.Unlock()
+			return storedPacket
+		}
+	}
+}
