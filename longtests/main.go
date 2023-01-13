6c140b6de3ed3a67911c58852da7f62a1b072e9d
malachyoconnor <malachyguitar@gmail.com>
2023-01-13 16:28:08 +0000

    🚧 wip(longtests): Beinning tests for longer times

---
+++ longtests/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"MQTT-GO/client"
+	"MQTT-GO/gobro"
+	"MQTT-GO/packets"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
+var (
+	doSubscribes = flag.Bool("continualSubscribe", false, "Continually subscribe")
+	runServer    = flag.Bool("continualSubscribe", false, "Start a server")
+)
+
+func main() {
+	switch true {
+	case *doSubscribes:
+		continualSubscribe()
+	case *runServer:
+		server := gobro.CreateServer()
+		server.StartServer()
+	}
+}
+
+const (
+	hoursToTest = 3
+)
+
+func continualSubscribe() {
+	client := client.CreateClient()
+	err := client.SetClientConnection("localhost", 8000)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = client.SendConnect()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = client.SendSubscribe(packets.TopicWithQoS{Topic: "continual"})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	message := []byte("This is my test message")
+	fmt.Println("Starting the test")
+	go func() {
+		time.Sleep((3600*hoursToTest + 100) * time.Second)
+		fmt.Println("We waited too long!! Exiting")
+		os.Exit(0)
+	}()
+
+	for i := 0; i < 3600*hoursToTest; i++ {
+		time.Sleep(time.Second)
+		err := client.SendPublish(append(message, byte(i)), "continual")
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	fmt.Println("Completed successfully")
+
+}
